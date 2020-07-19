2a9bd3a15643be57ed9a46cd8056e6bce991264e
Eason Chai <echai2905@gmail.com>
2020-07-19 22:13:05 +0800

    Created EstateAgent and removed other files

--- contracts/DecentramallToken.sol
+++ contracts/DecentramallToken.sol
@@ -1,75 +1,16 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.6.8;
 
-import "./OwnableERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract DecentramallToken is OwnableERC721 {
-    //Max limit of tokens to be minted
-    uint256 public currentLimit = 1000;
+contract DecentramallToken is ERC721 {
 
-    constructor() OwnableERC721("Decentramall Space Token", "SPACE", 500) public {
+    constructor() ERC721("Decentramall Space Token", "SPACE") public {
     }
 
-    /**
-     * @dev Get price of next token
-     *
-     * Assuming current bonding curve function of y = x^2 + basePrice
-     *
-     * Plug in value into function above
-     */
-    function price() public view returns(uint256) {
-        uint256 nextToken = totalSupply() + 1;
-
-        return ((nextToken ** 2) + basePrice);
-    }
-
-    /**
-     * @dev Buy a unique token
-     *
-     * One address can only hold one token as the token ID is based on a hashed version of the buyer's address
-     *
-     * The price of the token is based on a bonding curve function
-     */
-    function buy() public payable {
-        require(msg.value >= price(), "Not enough funds to purchase token!");
-        _safeMint(msg.sender, uint256(keccak256(abi.encodePacked(msg.sender))), "");
-    }
-
-    /**
-     * @dev Sell a unique token
-     *
-     * One address can only hold one token as the token ID is based on a hashed version of the buyer's address
-     *
-     * The price of the token is based on a bonding curve function
-     */
-    function sell() public payable {
-        _safeMint(msg.sender, uint256(keccak256(abi.encodePacked(msg.sender))), "");
-    }
-
-    /**
-     * @dev Change the currentLimit variable (Max supply)
-     *
-     * Only admin(s) can change this variable
-     */
-    function changeLimit(uint256 _limit) public onlyAdmin{
-        currentLimit = _limit;
-    }
-
-    /**
-     * @dev Add a new admin
-     *
-     * Only admin(s) can add new admin
-     */
-    function addAdmin(address _newAdmin) public onlyAdmin{
-        adminByAddress[_newAdmin] = true;
-    }
-
-    /**
-     * @dev Get currentLimit
-     *
-     * Only admin(s) can add new admin
-     */
-    function getLimit() public view returns(uint256){
-        return(currentLimit);
+    function mint(address purchaser) public returns(bool, uint256){
+        uint256 tokenId = uint256(keccak256(abi.encodePacked(purchaser)));
+        _safeMint(purchaser, tokenId, "");
+        return (true, tokenId);
     }
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -15,10 +15,35 @@ describe("TokenContract", function () {
     expect(await token.name()).to.equal("Decentramall Space Token");
     expect(await token.symbol()).to.equal("SPACE");
   });
-  it("Should allow me to change the limit ", async function () {
-    await token.changeLimit(1500);
-    expect(await token.getLimit()).to.equal(1500);
+  // it("Should allow me to change the limit ", async function () {
+  //   await token.changeLimit(1500);
+  //   expect(await token.getLimit()).to.equal(1500);
+  // });
+  // it("Should block others from changing", async function () {
+  //   const [owner, addr1] = await ethers.getSigners();
+  //   await expect(await token.connect(addr1).changeLimit(1500)).to.be.revertedWith("Not an admin!");
+  // });
+});
+
+describe("EstateAgent", function () {
+  let Agent;
+  let agent;
+
+  beforeEach(async function () {
+    Agent = await ethers.getContractFactory("EstateAgent");
+    agent = await Agent.deploy(100, 5);
+
+    await agent.deployed();
   });
+  it("Should return token address & details", async function () {
+    let token = await agent.tokenAddress();
+    expect(await token.name()).to.equal("Decentramall Space Token");
+    expect(await token.symbol()).to.equal("SPACE");
+  });
+  // it("Should allow me to change the limit ", async function () {
+  //   await token.changeLimit(1500);
+  //   expect(await token.getLimit()).to.equal(1500);
+  // });
   // it("Should block others from changing", async function () {
   //   const [owner, addr1] = await ethers.getSigners();
   //   await expect(await token.connect(addr1).changeLimit(1500)).to.be.revertedWith("Not an admin!");
