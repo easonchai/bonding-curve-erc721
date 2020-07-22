45f0b0753dbff0b85c6360704a82fbd2b117cd2e
Eason Chai <echai2905@gmail.com>
2020-07-22 21:32:21 +0800

    Remove redundant files

--- contracts/Registry.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.6.8;
-
-/**
-    @notice This contract will house the registry records of valid SPACE tokens as well as rental status and current tenants of
-    the rental unit
- */
-contract Registry{
-
-}
